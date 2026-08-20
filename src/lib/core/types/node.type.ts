c007a49b5711bc906e3c3fd3118d56afe2a94416
Gaston Meseri <gmeseri@Gastons-Mac-mini.local>
2026-08-20 22:27:24 +0200

    Mark actions as methods in exposed type

--- src/lib/core/types/node.type.ts
+++ src/lib/core/types/node.type.ts
@@ -1,26 +1,26 @@
 import type { Signal } from '@angular/core';
 
 export type NodeApi = {
-  set: (value: any) => void;
-  patch: (value: any) => void;
-  reset: (...args: [] | [value: any]) => void;
+  set(value: any): void;
+  patch(value: any): void;
+  reset(...args: [] | [value: any]): void;
   valid: Signal<boolean>;
   touched: Signal<boolean>;
-  markAsTouched: () => void;
-  markAsUntouched: () => void;
+  markAsTouched(): void;
+  markAsUntouched(): void;
   dirty: Signal<boolean>;
-  markAsDirty: () => void;
-  markAsPristine: () => void;
+  markAsDirty(): void;
+  markAsPristine(): void;
   disabled: Signal<boolean>;
-  disable: () => void;
-  enable: () => void;
+  disable(): void;
+  enable(): void;
   readonly: Signal<boolean>;
-  markAsReadonly: () => void;
-  markAsWritable: () => void;
+  markAsReadonly(): void;
+  markAsWritable(): void;
   hidden: Signal<boolean>;
-  hide: () => void;
-  show: () => void;
-  _setParent?: (parent: NodeApi | null) => void;
+  hide(): void;
+  show(): void;
+  _setParent?(parent: NodeApi | null): void;
 };
 
 export type Node = (() => any) & { api: NodeApi };
@@ -31,6 +31,6 @@ export interface NodeDefinitions {
 }
 export type NodeValue<TNode> = TNode extends () => infer TValue ? TValue : never;
 export type NodeSet<TNode> =
-  TNode extends { api: { set: (value: infer TValue) => void } } ? TValue : never;
+  TNode extends { api: { set(value: infer TValue): void } } ? TValue : never;
 export type NodePatch<TNode> =
-  TNode extends { api: { patch: (value: infer TValue) => void } } ? TValue : never;
+  TNode extends { api: { patch(value: infer TValue): void } } ? TValue : never;
