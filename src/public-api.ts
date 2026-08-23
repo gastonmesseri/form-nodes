ed25e422d6b1c2c6e4efb1da6d0a02f2038f4d4b
Gaston Meseri <gmeseri@Gastons-Mac-mini.local>
2026-08-23 22:33:37 +0200

    Sort public-api file lines

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,49 +1,49 @@
-export { field } from './lib/core/primitives/field';
-export type { Field, FieldApi, FieldOptions } from './lib/core/primitives/field';
-export { array } from './lib/core/primitives/array';
-export type { ArrayApi, ArrayIndexes, ArrayItemWithParent, ArrayItems, ArrayNode, ArrayOptions, ArrayPatch, ArraySet, ArrayValue } from './lib/core/primitives/array';
 export { form } from './lib/core/primitives/form';
-export { FORM_NODE, FormNodeDirective } from './lib/core/directives/form-node/form-node.directive';
-export { asyncValidator } from './lib/core/validation/async-validator';
-export type { AsyncValidatorOptions, ParameterizedAsyncValidatorConfig, ParameterizedAsyncValidatorOptions } from './lib/core/validation/async-validator';
-export type {
-  Form,
-  FormApi,
-  FormOptions,
-  FormPatch,
-  FormSet,
-  FormValue,
-} from './lib/core/primitives/form';
-export type { MarkAsTouchedOptions, Node, NodeApi } from './lib/core/types/node.type';
-export type { ObservableLike, ObserverLike, SubscriptionLike } from './lib/core/types/observable-like.type';
-export type {
-  FieldContext,
-  AsyncValidationResult,
-  AsyncValidator,
-  AsyncValidatorApi,
-  AsyncValidatorBaseContext,
-  AsyncValidatorContext,
-  AsyncValidatorState,
-  ComposableValidationResult,
-  ComposableValidator,
-  ParameterizedAsyncValidatorContext,
-  ValidationError,
-  ValidationResult,
-  ValidationSuccess,
-  Validator,
-  ValidatorApi,
-  ValidatorContext,
-  ValidatorReadonlyApi,
-  ValidatorSource,
-  Validators,
-  ValidationStatus,
-} from './lib/core/validation/validation.type';
+export { field } from './lib/core/primitives/field';
+export { array } from './lib/core/primitives/array';
 export { max } from './lib/core/validation/validators/max';
 export { min } from './lib/core/validation/validators/min';
 export { email } from './lib/core/validation/validators/email';
 export { pattern } from './lib/core/validation/validators/pattern';
-export { required, type RequiredOptions } from './lib/core/validation/validators/required';
 export { maxDate } from './lib/core/validation/validators/max-date';
 export { minDate } from './lib/core/validation/validators/min-date';
 export { maxLength } from './lib/core/validation/validators/max-length';
 export { minLength } from './lib/core/validation/validators/min-length';
+export { asyncValidator } from './lib/core/validation/async-validator';
+export type { Field, FieldApi, FieldOptions } from './lib/core/primitives/field';
+export type { MarkAsTouchedOptions, Node, NodeApi } from './lib/core/types/node.type';
+export { required, type RequiredOptions } from './lib/core/validation/validators/required';
+export { FORM_NODE, FormNodeDirective } from './lib/core/directives/form-node/form-node.directive';
+export type { ObservableLike, ObserverLike, SubscriptionLike } from './lib/core/types/observable-like.type';
+export type { AsyncValidatorOptions, ParameterizedAsyncValidatorConfig, ParameterizedAsyncValidatorOptions } from './lib/core/validation/async-validator';
+export type { ArrayApi, ArrayIndexes, ArrayItemWithParent, ArrayItems, ArrayNode, ArrayOptions, ArrayPatch, ArraySet, ArrayValue } from './lib/core/primitives/array';
+export type {
+  Form,
+  FormApi,
+  FormSet,
+  FormPatch,
+  FormValue,
+  FormOptions,
+} from './lib/core/primitives/form';
+export type {
+  Validator,
+  Validators,
+  ValidatorApi,
+  FieldContext,
+  AsyncValidator,
+  ValidatorSource,
+  ValidationError,
+  ValidationStatus,
+  ValidationResult,
+  ValidatorContext,
+  ValidationSuccess,
+  AsyncValidatorApi,
+  AsyncValidatorState,
+  ComposableValidator,
+  ValidatorReadonlyApi,
+  AsyncValidationResult,
+  AsyncValidatorContext,
+  AsyncValidatorBaseContext,
+  ComposableValidationResult,
+  ParameterizedAsyncValidatorContext,
+} from './lib/core/validation/validation.type';
