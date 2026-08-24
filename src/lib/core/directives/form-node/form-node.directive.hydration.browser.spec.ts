d81871b2573a8ef63ffe7f181156d81b0a6fe7f1
Gaston Meseri <gmeseri@Gastons-Mac-mini.local>
2026-08-24 23:33:40 +0200

    Improve readability in form-node directive

--- src/lib/core/directives/form-node/form-node.directive.hydration.browser.spec.ts
+++ src/lib/core/directives/form-node/form-node.directive.hydration.browser.spec.ts
@@ -8,7 +8,7 @@ import { required } from '../../validation/validators/required';
 import { FormNodeDirective } from './form-node.directive';
 import { registerSignalInputForJit } from '../../../../../testing/register-signal-input-for-jit';
 
-registerSignalInputForJit(FormNodeDirective, 'formNode', '_fieldInput');
+registerSignalInputForJit(FormNodeDirective, 'formNode', 'formNodeInput');
 
 declare const __FORM_NODE_HYDRATION_HTML__: string;
 
--- src/lib/core/directives/form-node/form-node.directive.spec.ts
+++ src/lib/core/directives/form-node/form-node.directive.spec.ts
@@ -16,7 +16,7 @@ import { required } from '../../validation/validators/required';
 import { registerSignalInputForJit } from '../../../../../testing/register-signal-input-for-jit';
 import { isNativeFormNodeControl, parseNativeControlValue, readNativeControlValue, writeNativeControlValue } from './native-control';
 
-registerSignalInputForJit(FormNodeDirective, 'formNode', '_fieldInput');
+registerSignalInputForJit(FormNodeDirective, 'formNode', 'formNodeInput');
 
 beforeAll(() => TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting()));
 afterAll(() => TestBed.resetTestEnvironment());
